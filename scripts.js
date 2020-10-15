de1046fcaf1d3c81ecf6bc2172a7505e7e342255
Tim Heck <tih99924@bethel.edu>
2020-10-15 13:30:11 -0500

    updated arrow and form

--- scripts.js
+++ scripts.js
@@ -6,20 +6,20 @@ function changeArrowFunction() {
     if (scrollbarPos < 7640) {
         arrow.style.display = 'none';
     } else {
-        arrow.style.display = 'block'
+        arrow.style.display = 'block';
     }
 
-    if (scrollbarPos < 12500) {
-        arrow.href = '#calabrio';
-    } else if (scrollbarPos < 20690) {
-        arrow.href = '#bsc';
-    } else if (scrollbarPos < 28390) {
-        arrow.href = '#csi';
-    } else if (scrollbarPos < 31610) {
-        arrow.href = '#timberlandpartners';
-    } else if (scrollbarPos < 37230) {
-        arrow.href = '#medica';
-    } else {
-        arrow.href = '#abbott';
-    }
+    // if (scrollbarPos < 12500) {
+    //     arrow.href = '#calabrio';
+    // } else if (scrollbarPos < 20690) {
+    //     arrow.href = '#bsc';
+    // } else if (scrollbarPos < 28390) {
+    //     arrow.href = '#csi';
+    // } else if (scrollbarPos < 31610) {
+    //     arrow.href = '#timberlandpartners';
+    // } else if (scrollbarPos < 37230) {
+    //     arrow.href = '#medica';
+    // } else {
+    //     arrow.href = '#abbott';
+    // }
 }
